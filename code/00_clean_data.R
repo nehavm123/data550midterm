2741dbfd347b9eda76acc2f0680f3095090a5365
Neil Gutlay <neilgutlay@gmail.com>
2024-11-13 18:19:47 -0500

    Code for 00_clean_data is now finished

--- code/00_clean_data.R
+++ code/00_clean_data.R
@@ -1,3 +1,18 @@
 library(here)
+library(dplyr)
 
+# Here Command
 here::i_am("code/00_clean_data.R")
+
+# Reads in Data
+covid_sub_location <- here::here("data/covid_sub.csv")
+covid <- read.csv(covid_sub_location, header = TRUE)
+
+# Create new variable to indicate if person has COVID-19
+covid <- covid %>% 
+  mutate(CASE_STATUS = ifelse(CLASIFFICATION_FINAL < 4, 1, 0),
+         DIED = ifelse(!is.na(DATE_DIED), 1, 0)
+  )
+
+# Save onto the output folder
+saveRDS(covid, file = here::here("output/data_clean.rds"))
